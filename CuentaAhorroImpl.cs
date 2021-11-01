f072f67626e8c078b5ddd966f5ffc65102a89239
rominamenacho <43242020+rominamenacho@users.noreply.github.com>
2021-10-31 21:40:27 -0300

    primer commit

---
+++ CuentaAhorroImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+   public class CuentaAhorroImpl : ICuenta
+    {
+        public string Tipo { get; set; }
+        public double Monto { get; set; }
+
+        public CuentaAhorroImpl()
+        {
+            Tipo = "AHORRO";
+        }
+
+       public ICuenta Clonar()
+        {
+            CuentaAhorroImpl cuenta = null;
+            try
+            {
+                cuenta = (CuentaAhorroImpl) MemberwiseClone();
+            }catch(Exception e)
+            {
+              Console.WriteLine( e.Message);
+            }
+            return cuenta;
+        }
+  
+      
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CuentaAhorroImpl cuentaUno = new CuentaAhorroImpl();
+            cuentaUno.Monto=2000;
+
+            CuentaAhorroImpl clonada = (CuentaAhorroImpl)cuentaUno.Clonar();
+
+            if (clonada != null) Console.WriteLine("Clonada existe");
+          
+
+            Console.WriteLine($"Cuenta Clonada tiene un monto de {clonada.Monto} ,Cuenta original tiene un monto de  {cuentaUno.Monto}");
+
+            Console.ReadKey();
+        }
+    }
+}
